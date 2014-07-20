f3d2f942dd411ce20cad53bdd3451b0095b80054
0xack13 <0xack13@gmail.com>
2014-07-20 17:18:48 +0300

    WF Extension

---
+++ Extensions/MyActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+
+namespace Extensions
+{
+
+    public sealed class MyActivity : CodeActivity
+    {
+        // Define an activity input argument of type string
+        public InArgument<string> Text { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            ExecutionCountExtension ext = context.GetExtension<ExecutionCountExtension>();
+            if (ext != null)
+            {
+                ext.Register(context.WorkflowInstanceId);
+            }
+
+        }
+    }
+}
---
+++ Extensions/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+using System.Collections.Generic;
+
+
+namespace Extensions
+{
+
+    // This extension collects a list of workflow Ids
+    public class ExecutionCountExtension
+    {
+        IList<Guid> instances = new List<Guid>();
+
+        public int ExecutionCount
+        {
+            get
+            {
+                return this.instances.Count;
+            }
+        }
+
+        public IEnumerable<Guid> InstanceIds
+        {
+            get
+            {
+                return this.instances;
+            }
+        }
+
+        public void Register(Guid activityInstanceId)
+        {
+            if (!this.instances.Contains<Guid>(activityInstanceId))
+            {
+                instances.Add(activityInstanceId);
+            }
+        }
+    }
+
+
+    class Program
+    {
+        // Creates a workflow that uses the activity that consumes the extension
+        static Activity CreateWorkflow1()
+        {
+            return new Sequence
+            {
+                Activities =
+            {
+                new MyActivity()
+            }
+            };
+        }
+
+        // Creates a workflow that uses two instances of the activity that consumes the extension
+        static Activity CreateWorkflow2()
+        {
+            return new Sequence
+            {
+                Activities =
+            {
+                new MyActivity(),
+                new MyActivity()
+            }
+            };
+        }
+
+        static void Main(string[] args)
+        {
+            // create the extension 
+            ExecutionCountExtension executionCountExt = new ExecutionCountExtension();
+
+            // configure the first invoker and execute 3 times
+            WorkflowInvoker invoker = new WorkflowInvoker(CreateWorkflow1());
+            invoker.Extensions.Add(executionCountExt);
+            invoker.Invoke();
+            invoker.Invoke();
+            invoker.Invoke();
+
+            // configure the second invoker and execute 2 times
+            WorkflowInvoker invoker2 = new WorkflowInvoker(CreateWorkflow2());
+            invoker2.Extensions.Add(executionCountExt);
+            invoker2.Invoke();
+            invoker2.Invoke();
+
+            // show the data in the extension
+            Console.WriteLine("Executed {0} times", executionCountExt.ExecutionCount);
+            executionCountExt.InstanceIds.ToList().ForEach(i => Console.WriteLine("...{0}", i));
+        }
+    }
+}
